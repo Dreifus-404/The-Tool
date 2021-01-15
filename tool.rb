022f4a17c97283161f4ac98afccedf1e21d23314
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-01-15 17:58:16 -0300

    Update tool.rb

--- tool.rb
+++ tool.rb
@@ -1,3 +1,6 @@
+################
+### C O D E ####
+################
 def b1()
   system "clear"
   puts "\n\n\n"
@@ -21,6 +24,7 @@ def ex()
   end
 def opc()
   puts "[1] Interpretadores"
+  puts "[2] Pacotes A Mais"
   puts "\n\n"
   puts "                     •Select• One Option"
   puts "\n\n"
@@ -38,7 +42,6 @@ if esc == 1
   puts "[3] Ruby"
   puts "[4] Perl"
   puts "[5] Php"
-  puts "[6] Git"
   puts "[99] •Instalar• Todos"
   puts "\n\n"
   puts "                     •Select• One Option"
@@ -81,3 +84,58 @@ if esc == 1
     b1()
     end
   end
+if esc == 2
+  b1()
+  puts "[1] Binutils"
+  puts "[2] Coreutils"
+  puts "[3] Dnsutils"
+  puts "[4] Openssh"
+  puts "[5] Wget"
+  puts "[6] Curl"
+  puts "[7] Zsh"
+  puts "[99] •Instalar• Todos"
+  puts "\n\n"
+  puts "           $ Select On Option"
+  puts "\n\n"
+  opc2 = gets.to_i
+  case opc2
+  when 1
+    system "clear"
+    system "apt install binutils -y"
+    b1()
+  when 2
+    system "clear"
+    system "apt install coreutils -y"
+    b1()
+  when 3
+    system "clear"
+    system "apt install dnsutils -y"
+    b1()
+  when 4
+    system "clear"
+    system "apt install openssh -y"
+    b1()
+  when 5
+    system "clear"
+    syatem "apt install wget -y"
+    b1()
+  when 6
+    system "clear"
+    system "apt install curl -y"
+    b1()
+  when 7
+    system "clear"
+    system "apt install zsh -y"
+    b1()
+  when 99
+    system "clear"
+    system "apt install binutils -y"
+    system "apt install coreutils -y"
+    system "apt install dnsutils -y"
+    system "apt install openssh -y"
+    system "apt install wget -y"
+    system "apt install curl -y"
+    system "apt install zsh -y"
+    b1()
+    end
+end
