f4089d21f1a38c028e6662ef0bf52d6521102909
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-01-18 22:37:40 -0300

    Update tool.rb

--- tool.rb
+++ tool.rb
@@ -1,6 +1,5 @@
 #C0D3~#
-def b1()
-  system "clear"
+def b1()                                                                      system "clear"
   puts "\n\n\n"
   puts "    █۞███████]▄▄▄▄▄▄▄▄▄▄▃"
   puts "  ▂▄▅█████████▅▄▃▂                              T H E"
@@ -10,10 +9,10 @@ def b1()
   puts "\n\n\n"
   end
 def ex()
-  puts "[~$] Ola Bem Vindo A Minha Tool De Instalaçāo"
-  puts "[~$] Essa Tool Serve Para Fazer Instalaçōes Basicas Para Seu Terminal Linux"
-  puts "[~$] Esta Tool Esta Bem Beta Fique De Olho Logo mais Tera Atualizações"
-  puts "[~$] Espero Que Goste, Aproveite"
+  puts "~$ Ola Bem Vindo A Minha Tool De Instalaçāo"
+  puts "~$ Essa Tool Serve Para Fazer Instalaçōes Basicas Para Seu Terminal Linux"
+  puts "~$ Esta Tool Esta Bem Beta Fique De Olho Logo mais Tera Atualizações"
+  puts "~$ Espero Que Goste, Aproveite"
   puts "\n\n"
   puts "                   [~$] Press Enter For Continue"
   puts "\n\n"
@@ -31,9 +30,10 @@ def fim()
   end
 def opc()
   puts "[1] Interpretadores"
-  puts "[2] Pacotes A Mais"
+  puts "[2] Coleta De Informações"
+  puts "[3] Pacotes A Mais"
   puts "\n\n"
-   puts "                     •Select• One Option"
+  puts "                     •Select• One Option"
   puts "\n\n"
   opc = gets.to_i
   return opc
@@ -49,7 +49,7 @@ if esc == 1
   puts "[3] Ruby"
   puts "[4] Perl"
   puts "[5] Php"
-  puts "[99] •Instalar• Todos"
+  puts "[00] •Instalar• Todos"
   puts "\n\n"
   puts "                     •Select• One Option"
   puts "\n\n"
@@ -91,7 +91,7 @@ if esc == 1
     slp()
     b1()
     fim()
-  when 99
+  when 00
     system "clear"
     system "apt update && apt upgrade -y"
     system "apt install python2 -y"
@@ -105,21 +105,82 @@ if esc == 1
     fim()
     end
   end
+
 if esc == 2
+  b1()
+  puts "[1] Nmap"
+  puts "[2] Openssh"
+  puts "[3] Wget"
+  puts "[4] Curl"
+  puts "[5] Dnsutils"
+  puts "[6] Exiftool"
+  puts "[00] Instalar Todos"
+  puts "\n\n"
+  puts "                     •Select• One Option"
+  puts "\n\n"
+  esc = gets.to_i
+  case esc
+  when 1
+    system "clear"
+    system "apt install nmap -y"
+    slp()
+    b1()
+    fim()
+  when 2
+    system "clear"
+    system "apt install openssh -y"
+    slp()
+    b1()
+    fim()
+  when 3
+    system "clear"
+    system "apt install wget -y"
+    slp()
+    b1()
+    fim()
+  when 4
+    system "clear"
+    system "apt install curl -y"
+    slp()
+    b1()
+    fim()
+  when 5
+    system "clear"
+    system "apt install dnsutils -y"
+    slp()
+    b1()
+    fim()
+  when 6
+    system "clear"
+    system "apt install exiftool -y"
+    slp()
+    b1()
+    fim()
+  when 00
+    system "clear"
+    system "apt install nmap -y"
+    system "apt install openssh -y"
+    system "apt install wget -y"
+    system "apt install curl -y"
+    system "apt install dnsutils -y"
+    system "apt install exiftool -y"
+    slp()
+    b1()
+    fim()
+    end
+  end
+
+if esc == 3
   b1()
   puts "[1] Binutils"
   puts "[2] Coreutils"
-  puts "[3] Dnsutils"
-  puts "[4] Openssh"
-  puts "[5] Wget"
-  puts "[6] Curl"
-  puts "[7] Zsh"
-  puts "[99] •Instalar• Todos"
+  puts "[3] Zsh"
+  puts "[00] •Instalar• Todos"
   puts "\n\n"
-  puts "           $ Select On Option"
+  puts "                     •Select• One Option"
   puts "\n\n"
-  opc2 = gets.to_i
-  case opc2
+  esc = gets.to_i
+  case esc
   when 1
     system "clear"
     system "apt install binutils -y"
@@ -134,42 +195,14 @@ if esc == 2
     fim()
   when 3
     system "clear"
-    system "apt install dnsutils -y"
-    slp()
-    b1()
-    fim()
-  when 4
-    system "clear"
-    system "apt install openssh -y"
-    slp()
-    b1()
-    fim()
-  when 5
-    system "clear"
-    syatem "apt install wget -y"
-    slp()
-    b1()
-    fim()
-  when 6
-    system "clear"
-    system "apt install curl -y"
-    slp()
-    b1()
-    fim()
-  when 7
-    system "clear"
     system "apt install zsh -y"
     slp()
     b1()
     fim()
-  when 99
+  when 00
     system "clear"
-    system "apt install binutils -y"
+    system "apt install  binutils -y"
     system "apt install coreutils -y"
-    system "apt install dnsutils -y"
-    system "apt install openssh -y"
-    system "apt install wget -y"
-    system "apt install curl -y"
     system "apt install zsh -y"
     slp()
     b1()
